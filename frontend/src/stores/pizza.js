4ecbfef97477889fb3e20e7f6f3fb8173645e8f5
Danila Avetisyan <avetisyandanila21@yandex.ru>
2023-02-28 13:43:09 +0300

    Добавил геттеры и доп. функции хелперы для геттеров

--- frontend/src/stores/pizza.js
+++ frontend/src/stores/pizza.js
@@ -1,4 +1,7 @@
 import { defineStore } from "pinia";
+import { ingredientsQuantity } from "@/common/helpers/ingredients-quantity";
+import { pizzaPrice } from "@/common/helpers/pizza-price";
+import { useDataStore } from "@/stores/data";
 
 export const usePizzaStore = defineStore("pizza", {
     state: () => ({
@@ -9,6 +12,37 @@ export const usePizzaStore = defineStore("pizza", {
         sizeId: 0,
         ingredients: [],
     }),
-    getters: {},
+    getters: {
+        sauce: (state) => {
+            const data = useDataStore();
+            return data.sauces.find((item) => item.id === state.sauceId) ?? data.sauces[0];
+        },
+        dough: (state) => {
+            const data = useDataStore();
+            return data.doughs.find((item) => item.id === state.doughId) ?? data.doughs[0];
+        },
+        size: (state) => {
+            const data = useDataStore();
+            return data.sizes.find((item) => item.id === state.sizeId) ?? data.sizes[0];
+        },
+        ingredientsExtended: (state) => {
+            const data = useDataStore();
+            const pizzaIngredientsIds = state.ingredients.map((item) => item.ingredientId);
+            return data.ingredients
+                .filter((item) => pizzaIngredientsIds.includes(item.id))
+                .map((item, index) => {
+                    return {
+                        ...item,
+                        quantity: state.ingredients[index]?.quantity ?? 0,
+                    };
+                });
+        },
+        price: (state) => {
+            return pizzaPrice(state);
+        },
+        ingredientQuantities: (state) => {
+            return ingredientsQuantity(state);
+        },
+    },
     actions: {}
 });
